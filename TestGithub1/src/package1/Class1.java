608c3800dfcfab00217c89511b7ac2d0fd420fba
lx-2015 <213h001@gmail.com>
2017-03-30 17:20:47 -0700

    Add fun2 in package1 Class1 directly on Github

--- TestGithub1/src/package1/Class1.java
+++ TestGithub1/src/package1/Class1.java
@@ -2,7 +2,11 @@ package package1;
 
 public class Class1 {
 	public void fun1() {
-		System.out.println("package1's Class1.");
+		System.out.println("package1 Class1 fun1.");
+	}
+	
+	public void fun2() {
+		System.out.println("package1 Class1 fun2.");
 	}
 
 	public static void main(String[] args) {
