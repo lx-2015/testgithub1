36c18c6852d82ddbf1445972e5d7db747b8e7d0b
lx-2015 <213h001@gmail.com>
2017-03-30 17:50:54 -0700

    Add fun4 and modify main function

--- TestGithub1/src/package1/Class1.java
+++ TestGithub1/src/package1/Class1.java
@@ -13,11 +13,15 @@ public class Class1 {
 		System.out.println("package1 Class1 fun3.");
 	}
 
+	public static void fun4() {
+		System.out.println("package1 Class1 fun4.");
+	}
+	
 	public static void main(String[] args) {
 		fun1();
 		fun2();
 		fun3();
-
+		fun4();
 	}
 
 }
