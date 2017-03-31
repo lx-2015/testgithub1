eff72f80cbc432006e614e95f968827a69600317
XiangLi <li.984@buckeyemail.osu.edu>
2017-03-30 17:49:07 -0700

    Add fun3 and modify main function

--- TestGithub1/src/package1/Class1.java
+++ TestGithub1/src/package1/Class1.java
@@ -1,16 +1,22 @@
 package package1;
 
 public class Class1 {
-	public void fun1() {
+	public static void fun1() {
 		System.out.println("package1 Class1 fun1.");
 	}
 	
-	public void fun2() {
+	public static void fun2() {
 		System.out.println("package1 Class1 fun2.");
+	}
+	
+	public static void fun3() {
+		System.out.println("package1 Class1 fun3.");
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		fun1();
+		fun2();
+		fun3();
 
 	}
 
