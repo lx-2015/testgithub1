fb43c2994351ee0a934fada01c4ad78b26c34bb3
XiangLi <li.984@buckeyemail.osu.edu>
2017-03-30 17:08:43 -0700

    Initial commit

---
+++ TestGithub1/src/package1/Class1.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class Class1 {
+	public void fun1() {
+		System.out.println("package1's Class1.");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
